2d3a28a92cd9788bb3eee6a4bec8069e884c7630
Sergey Orlov <sorlov@redhat.com>
2020-11-25 10:45:39 +0100

    temporary disable collecting logs for each test case

--- ipatests/test_integration/base.py
+++ ipatests/test_integration/base.py
@@ -28,7 +28,7 @@ from pytest_sourceorder import ordered
 
 @ordered
 @pytest.mark.usefixtures('mh')
-@pytest.mark.usefixtures('integration_logs')
+# @pytest.mark.usefixtures('integration_logs')
 class IntegrationTest:
     num_replicas = 0
     num_clients = 0
